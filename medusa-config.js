0a37dc809ee73f9e5855187b6c13c720d848c14a
delicloredana <loredana.delic@agilo.co>
2025-07-14 16:04:39 +0200

    refactor: simplify medusa config by removing unused variables

--- medusa-config.js
+++ medusa-config.js
@@ -1,32 +1,20 @@
 const { defineConfig, Modules } = require('@medusajs/utils');
 
-const DB_URL = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:/medusa-analytics-test`;
-process.env.DATABASE_URL = DB_URL;
-process.env.LOG_LEVEL = 'error';
-
 module.exports = defineConfig({
   admin: {
     disable: true,
   },
-  projectConfig: {
-    http: {
-      jwtSecret: process.env.JWT_SECRET || 'test',
-    },
-  },
   modules: [
     {
-      resolve: "@medusajs/medusa/fulfillment",
+      resolve: '@medusajs/medusa/fulfillment',
       options: {
         providers: [
-       
           {
-            resolve: "@medusajs/fulfillment-manual", 
-            id: "test-provider",                  
-        
+            resolve: '@medusajs/fulfillment-manual',
+            id: 'test-provider',
           },
         ],
       },
     },
   ],
-
 });

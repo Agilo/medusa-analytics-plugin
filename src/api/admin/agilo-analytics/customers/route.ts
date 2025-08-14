78828cdad2d4ff802b7af97022517a5bd5668ae9
delicloredana <loredana.delic@agilo.co>
2025-08-14 16:18:49 +0200

    feat: total customers

---
+++ src/api/admin/agilo-analytics/customers/route.ts
@@ -0,0 +1,49 @@
+import { MedusaRequest, MedusaResponse } from '@medusajs/framework';
+import {
+  ContainerRegistrationKeys,
+  MedusaError,
+} from '@medusajs/framework/utils';
+import { z } from 'zod';
+
+export const adminCustomerAnalyticsQuerySchema = z.object({
+  date_from: z.string(),
+  date_to: z.string(),
+});
+
+export async function GET(req: MedusaRequest, res: MedusaResponse) {
+  const result = adminCustomerAnalyticsQuerySchema.safeParse(req.query);
+  const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
+
+  if (!result.success) {
+    throw new MedusaError(
+      MedusaError.Types.INVALID_DATA,
+      result.error.errors.map((err) => err.message).join(', ')
+    );
+  }
+  const { data: orders } = await query.graph({
+    entity: 'order',
+    fields: ['id', 'created_at', 'customer.*', 'customer.orders.*'],
+    filters: {
+      created_at: {
+        $gte: result.data.date_from,
+        $lte: result.data.date_to,
+      },
+      status: { $nin: ['draft', 'canceled'] },
+    },
+  });
+
+  const customers = Object.values(
+    orders.reduce((acc, { customer }) => {
+      if (customer && !acc[customer.id]) {
+        acc[customer.id] = customer;
+      }
+      return acc;
+    }, {})
+  );
+
+  const customerData = {
+    total_customers: customers.length,
+  };
+
+  res.json(customerData);
+}

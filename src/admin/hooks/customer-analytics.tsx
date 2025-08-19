834ffd8b98ba864658d73479cb94a97ba29d07ed
delicloredana <loredana.delic@agilo.co>
2025-08-19 13:37:05 +0200

    feat: implement customer analytics data retrieval functionality

---
+++ src/admin/hooks/customer-analytics.tsx
@@ -0,0 +1,39 @@
+import { DateRange } from 'react-day-picker';
+import { useQuery } from '@tanstack/react-query';
+import type { UseQueryOptions } from '@tanstack/react-query';
+import { retrieveCustomersAnalytics } from '../lib/data/customer-analytics';
+
+export type CustomerAnalyticsResponse = {
+  total_customers: number;
+  new_customers: number;
+  returning_customers: number;
+  customer_count: { name: string; count: number }[];
+  customer_group: { name: string; count: number }[];
+  customer_sales: {
+    customer_id: string;
+    sales: number;
+    name: string;
+    groups: string[];
+    count: number;
+    last_order: Date;
+    email: string;
+  }[];
+  currencyCode: string;
+};
+
+export const useCustomerAnalytics = (
+  query: DateRange | undefined,
+  options?: Omit<
+    UseQueryOptions<CustomerAnalyticsResponse | undefined, Error>,
+    'queryKey' | 'queryFn'
+  >
+) => {
+  return useQuery({
+    queryKey: ['customer-analytics', query?.from, query?.to],
+    queryFn: async () => {
+      const data = await retrieveCustomersAnalytics(query);
+      return data;
+    },
+    ...options,
+  });
+};

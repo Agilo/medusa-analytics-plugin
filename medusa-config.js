42cb438568fa4b79cb48d512e0bf662b94bd8809
delicloredana <loredana.delic@agilo.co>
2025-09-01 12:33:23 +0200

    add redis to medusa config

--- medusa-config.js
+++ medusa-config.js
@@ -1,8 +1,18 @@
 const { defineConfig, Modules } = require('@medusajs/utils');
+const dotenv = require('dotenv');
+const path = require('path');
 
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: path.resolve(__dirname, '.env.test') });
+} else {
+  dotenv.config();
+}
 module.exports = defineConfig({
   admin: {
     disable: true,
+  },
+  projectConfig: {
+    redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
   },
   modules: [
     {

70da0c85e4b2e9dfee6341e82936ba41d5eac7c5
Ante Primorac <ante@agilo.com>
2026-03-06 09:45:15 +0100

    fix: update import statements for ProductAnalyticsResponse type

--- src/admin/hooks/product-analytics.tsx
+++ src/admin/hooks/product-analytics.tsx
@@ -2,7 +2,7 @@ import { DateRange } from 'react-day-picker';
 import { useQuery } from '@tanstack/react-query';
 import type { UseQueryOptions } from '@tanstack/react-query';
 import { retrieveProductAnalytics } from '../lib/data/product-analytics';
-import { ProductAnalyticsResponse } from '../../api/admin/agilo-analytics/products/route';
+import type { ProductAnalyticsResponse } from '../../api/admin/agilo-analytics/products/route';
 
 export const useProductAnalytics = (
   query: DateRange | undefined,
--- src/admin/lib/data/product-analytics.ts
+++ src/admin/lib/data/product-analytics.ts
@@ -1,7 +1,7 @@
 import { DateRange } from 'react-day-picker';
 import { format } from 'date-fns';
-import { ProductAnalyticsResponse } from '../../hooks/product-analytics';
 import { sdk } from '../utils';
+import type { ProductAnalyticsResponse } from '../../../api/admin/agilo-analytics/products/route';
 
 export async function retrieveProductAnalytics(date: DateRange | undefined) {
   if (!date || !date.from || !date?.to) {
